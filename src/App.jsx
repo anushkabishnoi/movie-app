9e0b0aa263b8a83479b93634966eb060db52bdfc
Anushka Bishnoi <anushkabishnoi9@gmail.com>
2023-07-29 22:50:49 +0530

    comments removed

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ import pageNotFound from "./pages/404/pageNotFound";
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
-  console.log(url);
 
   useEffect(() => {
     fetchApiConfig();
@@ -24,7 +23,6 @@ function App() {
 
   const fetchApiConfig = () => {
     fetchDataFromApi("/configuration").then((res) => {
-      console.log(res);
 
       const url = {
         backdrop: res.images.secure_base_url + "original",
